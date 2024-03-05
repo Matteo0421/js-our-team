08d481cdd57bead1567e255102c818b59266e7b1
Matteo0421 <matteo.carola99@gmail.com>
2024-03-05 17:25:51 +0100

    [add]

--- js/script.js
+++ js/script.js
@@ -1,38 +1,41 @@
+
+const output = document.querySelector(".output")
+
 const myTeams = [
   {
-    Name : 'Wayne Barnett',
+    name : 'Wayne Barnett',
     jobs : 'Founder & CEO',
-    img : 'our-team/img/wayne-barnett-founder-ceo.jpg'
+    img : 'wayne-barnett-founder-ceo.jpg'
   },
 
   {
     Name : 'Angela Caroll',
     jobs : 'Chief Editor',
-    img : 'our-team/img/angela-caroll-chief-editor.jpg'
+    img : 'angela-caroll-chief-editor.jpg'
   },
 
   {
     Name : 'Walter Gordon',
     jobs : 'Office Manager',
-    img : 'our-team/img/walter-gordon-office-manager.jpg'
+    img : 'walter-gordon-office-manager.jpg'
   },
 
   {
     Name : 'Angela Lopez',
     jobs : 'Social Media Manager',
-    img : 'our-team/img/angela-lopez-social-media-manager.jpg'
+    img : 'angela-lopez-social-media-manager.jpg'
   },
 
   {
     Name : 'Scott Estrada',
     jobs : 'Developer',
-    img : 'our-team/img/scott-estrada-developer.jpg'
+    img : 'scott-estrada-developer.jpg'
   },
 
   {
     Name : 'Barbara Ramos',
     jobs : 'Graphic Designer',
-    img : 'our-team/img/barbara-ramos-graphic-designer.jpg'
+    img : 'barbara-ramos-graphic-designer.jpg'
   }
 ]
 
@@ -40,4 +43,22 @@ console.log(myTeams);
 
 for(let team of myTeams){
   console.log(team);
+
+    
+  // let listaNomi = '';
+  // for(let listaNomi of myTeams[0]){
+  //   listaNomi += `${myTeams[0]}`
+  // }
+
+
+  // output.innerHTML += `
+  // <div class="card me-5 " style="width: 18rem;">
+  //       <img src="img/${myTeams.img}" alt="...">
+  //       <div class="card-body">
+  //         <h3>${name}</h3>
+  //         <span>${jobs}</span>
+  //       </div>
+  //     </div>
+  // `
 }
+  
