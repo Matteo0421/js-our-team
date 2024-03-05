dfb6157872b9cfd1e4d262890263b1847779b0e5
Matteo0421 <matteo.carola99@gmail.com>
2024-03-05 16:01:14 +0100

    [add]minestone-1

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const myTeams = [
+  {
+    Name : 'Wayne Barnett',
+    jobs : 'Founder & CEO',
+    img : 'our-team/img/wayne-barnett-founder-ceo.jpg'
+  },
+
+  {
+    Name : 'Angela Caroll',
+    jobs : 'Chief Editor',
+    img : 'our-team/img/angela-caroll-chief-editor.jpg'
+  },
+
+  {
+    Name : 'Walter Gordon',
+    jobs : 'Office Manager',
+    img : 'our-team/img/walter-gordon-office-manager.jpg'
+  },
+
+  {
+    Name : 'Angela Lopez',
+    jobs : 'Social Media Manager',
+    img : 'our-team/img/angela-lopez-social-media-manager.jpg'
+  },
+
+  {
+    Name : 'Scott Estrada',
+    jobs : 'Developer',
+    img : 'our-team/img/scott-estrada-developer.jpg'
+  },
+
+  {
+    Name : 'Barbara Ramos',
+    jobs : 'Graphic Designer',
+    img : 'our-team/img/barbara-ramos-graphic-designer.jpg'
+  }
+]
+
+console.log(myTeams);
