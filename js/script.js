1aee78a9ec5161f2076ff5486a6b94fb0dbb39e0
Matteo0421 <matteo.carola99@gmail.com>
2024-03-05 18:12:35 +0100

    [end]

--- js/script.js
+++ js/script.js
@@ -41,45 +41,13 @@ const myTeams = [
 
 console.log(myTeams);
 
-// for(let team of myTeams){
-//   console.log(team);
 
-  
-//   let outputUser = '';
-//   for(let key in team){
-//     const outputUs = `${key}: ${myTeams[key]}`;
-//     console.log(outputUs);
-//     outputUser += outputUs + ' ';
-//   }
-
-
-
-
-//   output.innerHTML += `
-//   <div class="card me-5 " style="width: 18rem;">
-//         <img src="img/${myTeams.img}" alt="...">
-//         <div class="card-body">
-//           <h3>${myTeams.name}</h3>
-//           <span>${myTeams.jobs}</span>
-//         </div>
-//       </div>
-//   `
-// }
-
-// ciclo for of
-// stampo tutte le proprità deggli oggetti dellarry
 for(let myTeam of myTeams){
   console.log(myTeam);
-  // let outputUser = '';
-  // for(let key in myTeam){
-  //   const outputUs = `${key}: ${myTeam[key]}`;
-  //   console.log(outputUs);
-  //   outputUser += outputUs + ' ';
-  //   console.log(key);
-  // }
+ 
 
   output.innerHTML += `
-  <div class="card me-5 " style="width: 18rem;">
+  <div class="card my-card me-5 " style="width: 18rem;">
         <img src="img/${myTeam.img}" alt="...">
         <div class="card-body">
           <h3>${myTeam.name}</h3>
